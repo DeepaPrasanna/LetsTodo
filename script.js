c2fc048dea8073b71c80146fdcb0fcf7945b3673
DeepaPrasanna <anneraj73@gmail.com>
2020-09-04 23:08:20 +0530

    adding todos in local storage and removing the completed ones

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-""
 let remove = document.getElementById('remove')
 remove.addEventListener('click', removeTodos)
 
@@ -8,68 +7,110 @@ add.addEventListener('click', addTodos)
 let controls = document.querySelector('.container')
 let ul = document.getElementById('list')
 
+const KEY = "TODO_KEY"
+todos = []
+
+//check for any todos present in local storage
+retrivedTodos = JSON.parse(localStorage.getItem(KEY))
+if (retrivedTodos)
+{
+    todos = retrivedTodos
+}
+
 
 function removeTodos() {
+    
     //removing only the todos which are checked
     let li = ul.children;
-    // console.log(li)
 
     for (let index = 0; index < li.length; index++) {
 
         while (li[index] && li[index].children[0].checked) {
-            // console.log(li[index])
+            
+            text = li[index].children[1].innerText
+            removeIndex = todos.indexOf(text)
+            todos.splice(removeIndex,1)
+
+            //update the local storage
+            localStorage.setItem(KEY, JSON.stringify(todos))
+
             ul.removeChild(li[index])
         }
     }
 }
 
 function addTodos() {
+
     let input = document.getElementById('input')
     var todoText = input.value
-    var todoTextNode = document.createTextNode(todoText)
-
+      
+    // Prompt the user to enter a todo again if he enters an empty todo
     if (todoText === "") {
-        //add a p tag and assign a value of enter your todo
-        let enterTodo = document.createElement('p')
-        enterTodoText = document.createTextNode('Please enter a todo!')
-        enterTodo.appendChild(enterTodoText);
-        controls.appendChild(enterTodo)
-        //the element auto destroys itself
-        setTimeout(() => {
-            enterTodo.remove()
-        }, 2000)
 
+        alert("pls enter a todo")
     }
     else {
-        // enterTodo.remove()
-        //create li
+    
+        // check if the browser supports local storage
+        if(typeof(Storage) !== "undefined") {
 
-        let li = document.createElement('li')
+            // store the todo in an array
+            todos.push(todoText)
 
-        //create checkbox
-        let checkbox = document.createElement('input')
-        checkbox.type = 'checkbox'
-        checkbox.name = 'checkbox'
-        checkbox.setAttribute('id', 'check')
+            // store the todo data in local storage
+            localStorage.setItem(KEY, JSON.stringify(todos))
+        } else {
+            alert("Sorry, your browser does not support web storage...")
+        }
 
-        //create label
-        let label = document.createElement('label')
-
-        //add these elements
-
-        li.appendChild(checkbox)
-        label.appendChild(todoTextNode)
-        li.appendChild(label)
-        ul.insertBefore(li, ul.childNodes[0])
-
-        setTimeout(() => {
-            li.className = 'visual'
-        }, 2)
-        input.value = ""
+        // render all the todos
+        displayTodos();
+  
     }
-
-    window.localStorage.setItem("key", todoText);
-
-    // console.log((parseInt(key)) += 1)
-
 }
+
+function displayTodos()
+{
+    retrivedData = JSON.parse(localStorage.getItem(KEY))
+    
+   // As long as <ul> has a child node, remove it
+    while (list.hasChildNodes()) {  
+        list.removeChild(list.firstChild);
+    }
+    
+    retrivedData.forEach(element => {
+         
+        //create li
+         let li = document.createElement('li')
+
+         //create checkbox
+         let checkbox = document.createElement('input')
+         checkbox.type = 'checkbox'
+         checkbox.name = 'checkbox'
+         checkbox.setAttribute('id', 'check')
+ 
+         //create label
+         let label = document.createElement('label')
+ 
+         //add these elements
+ 
+         li.appendChild(checkbox)
+         //retrive the todo text from the local storage
+        //  let getTodoText = localStorage.getItem(KEY);
+         var todoTextNode = document.createTextNode(element)
+ 
+         label.appendChild(todoTextNode)
+         li.appendChild(label)
+         ul.insertBefore(li, ul.childNodes[0])
+ 
+         setTimeout(() => {
+             li.className = 'visual'
+         }, 2)
+         
+    });
+    
+    input.value = ""
+}
+
+// on window loading, display all the todos in the local storage, if any
+window.onload = displayTodos()
